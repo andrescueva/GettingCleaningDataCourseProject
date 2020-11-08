23153e15e55c02a06f1e87267387ecca88137409
AndresCuevaHome <anndres.cueva@gmail.com>
2020-11-08 10:14:50 -0500

    Create a tidy dataset

--- run_analysis.R
+++ run_analysis.R
@@ -55,3 +55,9 @@ colnames(dataset)<-labels
 #5.From the data set in step 4, creates a second, 
   #independent tidy data set with the average of each variable for 
   #each activity and each subject
+
+library(reshape2)
+datasetMelt = melt(dataset, id=c("Subject","Activity"),measure.vars = labels[3:length(labels)])
+datasetTidy <- dcast(datasetMelt, Subject + Activity ~ variable, mean)
+write.table(datasetTidy, file="../tidydataset.txt",row.names = FALSE)
+
