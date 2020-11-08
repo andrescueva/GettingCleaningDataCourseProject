49fe119165a3a68eb35d8a647c41cfc878167324
AndresCuevaHome <anndres.cueva@gmail.com>
2020-11-07 20:28:53 -0500

    Download dataset and unzip it.

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,17 @@
 #1.Merges the training and the test sets to create one data set.
+### Download dataset and unzip
+fileUrl    <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+dirDataset <- "./dataset"
+if (!file.exists(dirDataset)){
+  dir.create(dirDataset)
+}
+datasetZip <- paste0(dirDataset, "/dataset.zip")
+print(datasetZip)
+download.file(fileUrl, destfile = datasetZip, method = "curl")
+list.files(dirDataset)
+unzip(datasetZip, exdir=dirDataset, overwrite = TRUE)
+
+
 #2.Extracts only the measurements on the mean and standard deviation 
   #for each measurement.
 #3.Ues descriptive activity names to name the activities in the data set
